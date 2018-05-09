0d78ea4d1e0339fed77eabf54714ff3fd6a80259
Sinfool <megaplayz8@gmail.com>
2018-05-09 18:55:22 +1000

    added v1.0 files
    PASTE PASTE PASTE PASTE END ME I WANT TO DIE PASTE PASTE I FUCKED UR THOT

---
+++ MetaInfo.h
@@ -0,0 +1,8 @@
+
+#pragma once
+
+#define CORRUPTION_META_GAME "Counter-Strike: Global Offensive"
+#define CORRUPTION_META_CHEATVER "1.0.2"
+#define CORRUPTION_META_CHEATNAME "i_want_to_die.sin"
+
+void PrintMetaHeader();
